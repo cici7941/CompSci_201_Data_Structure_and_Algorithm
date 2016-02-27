233cda0d87d6125423a8712f38a32878e2ede7d5
Yaqian Cheng <cici_1115@hotmail.com>
2016-02-26 20:54:57 -0500

    updata TheBestName

--- APT3/src/TheBestName.java
+++ APT3/src/TheBestName.java
@@ -1,30 +1,33 @@
-import java.util.Comparator;
 import java.util.Arrays;
+import java.util.Comparator;
 
-public class TheBestName { 
-	public class Sorter implements Comparator<String>{
-		public int compare(String a, String b){
-		    if(a.equals("JOHN")){
-		    	return -1;
-		    } 
-		    else if(b.equals("JOHN")){
-		    	return 1;
-		    }
-			else if(weight(a) != weight(b)){
-				return weight(b) - weight(a);
-			} else
-				return a.compareTo(b);
+public class TheBestName {
+	public String[] sort(String[] names) {
+
+		class LengthAlpha implements Comparator<String> {
+	
+			public int compare(String o1, String o2) {
+				if(o1.equals("JOHN")){
+					return -1;
+				}
+				if(o2.equals("JOHN")){
+					return 1;
+				}
+				if (weight(o1) != weight(o2)) {
+					return weight(o2) - weight(o1);
+				} else {
+					return o1.compareTo(o2);
+				}
+			}	
+		}	
+	Arrays.sort(names, new LengthAlpha());
+	return names;
+}
+	public int weight(String o){
+		int weight1 = 0;
+		for(int i = 0; i <o.length(); i++){
+			weight1 += o.charAt(i) - 64;
 		}
-	}
-	public int weight(String name){
-		   int weight = 0;
-		   for(int i = 0; i < name.length(); i++){
-			   weight += ((int)name.charAt(i) - 64);
-		   }
-		   return weight;
-	 }
-	public String[] sort(String[] names) { 
-		Arrays.sort(names, new Sorter());
-		return names;
+	return weight1;
 	}
 }
